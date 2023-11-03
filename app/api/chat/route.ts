625a389277269e81df1ed6f89ed8513ccca20030
Younger Jo <youngerjo@gmail.com>
2023-11-04 00:04:35 +0900

    Remove redundent CORS middleware for demo project

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,5 @@
 import OpenAI from "openai"
 import { OpenAIStream, StreamingTextResponse } from "ai"
-import cors from "./cors"
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -8,32 +7,25 @@ const openai = new OpenAI({
 
 export const runtime = "edge"
 
-export function OPTIONS(request: Request) {
-  return cors()(request)
-}
-
 export async function POST(req: Request) {
-  const handler = cors(async (req) => {
-    const json = await req.json()
-    const { model, messages, user } = json as OpenAI.Chat.CompletionCreateParams
+  const json = await req.json()
+  const { model, messages, user } =
+    json as OpenAI.ChatCompletionCreateParamsStreaming
 
-    const params = {
-      model: model ?? "gpt-3.5-turbo",
-      max_tokens: 256,
-      temperature: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    }
+  const params = {
+    model: model ?? "gpt-3.5-turbo",
+    max_tokens: 256,
+    temperature: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  }
 
-    const response = await openai.chat.completions.create({
-      ...params,
-      messages,
-      user,
-      stream: true,
-    })
-
-    return new StreamingTextResponse(OpenAIStream(response))
+  const response = await openai.chat.completions.create({
+    ...params,
+    messages,
+    user,
+    stream: true,
   })
 
-  return handler(req)
+  return new StreamingTextResponse(OpenAIStream(response))
 }
